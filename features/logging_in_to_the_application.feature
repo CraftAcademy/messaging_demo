b00d36c334e1f6380df3b0d23eb465b4c2b9809b
faraznaeem <faraznaeem@gmail.com>
2018-09-18 14:20:57 +0200

    i have added a feature file for the login and some steps

---
+++ features/logging_in_to_the_application.feature
@@ -0,0 +1,20 @@
+Feature: Login to the app
+
+as a user
+in order to use the application
+I would like to login to my account
+
+
+Background:
+Given the following user exists
+|email|password|
+|faraz@craftacademy.se|mypassword|
+
+Scenario:
+Given I am on the landing page
+When I click on the link "login"
+And I fill in the "Email" with "faraz@craftacademy.se"
+And I fill in the "Password" with "mypassword"
+When I click on the link "Log in"
+Then I should be on the root page
+And I should see message "Signed in successfully"
---
+++ features/step_definitions/all_my_step_definitions_step.rb
@@ -0,0 +1,24 @@
+Given("the following user exists") do |table|
+    # table is a Cucumber::MultilineArgument::DataTable
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given("I am on the landing page") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("I click on the link {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("I fill in the {string} with {string}") do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I should be on the root page") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I should see message {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
