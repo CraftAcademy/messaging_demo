83e84e571df2b0b4d73c4428ab3c3f4a2491a9f5
oliverochman <oliver.ochman@hotmail.com>
2019-07-03 14:35:14 +0200

    finish everything and test

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -28,5 +28,5 @@ Then("I click on the {string} button") do |element|
 end
 
 Then("I should see {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  
 end
